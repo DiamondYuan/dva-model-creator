4766f6accc709c2f1f2f19eff5047fe52e2e8796
angular-moon <xiaoxuss@163.com>
2019-12-02 10:13:44 +0800

    change warning import for rollup build

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { call, put, select, take, cancel, all, race } from 'redux-saga/effects';
 import { Action, ActionCreator, PollActionCreator } from './actionCreatorFactory';
-import * as warning from 'warning';
+import warning from 'tiny-warning/dist/tiny-warning.esm';
 import { History } from 'history';
 import { Dispatch } from 'redux';
 
